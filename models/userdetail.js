d4ffb143debd4d5378026db6b7c57e02e6bf5e64
SatkarNiraula <satkarniraula69@gmail.com>
2024-08-23 10:23:21 +0545

    update :: on delete cascade middleware for userdetail

--- models/userdetail.js
+++ models/userdetail.js
@@ -37,6 +37,30 @@ const userdetail = new Schema({
   },
 });
 
+// Middleware to cascade delete all related documents when a userdetail is deleted
+userdetail.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function (next) {
+    try {
+      const userdetailId = this._id;
+
+      // Delete related documents from all relevant collections
+      await Promise.all([
+        RESUMEDB.Skills.deleteMany({ userdetail: userdetailId }),
+        RESUMEDB.Education.deleteMany({ userdetail: userdetailId }),
+        RESUMEDB.Experience.deleteMany({ userdetail: userdetailId }),
+        RESUMEDB.Projects.deleteMany({ userdetail: userdetailId }),
+        RESUMEDB.Objective.deleteMany({ userdetail: userdetailId }),
+      ]);
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 const UserDetail = mongoose.model("userdetail", userdetail);
 
 export default UserDetail;
