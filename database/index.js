ebcab689e516328436f8cfed24313815c4c091e6
SatkarNiraula <satkarniraula69@gmail.com>
2024-08-26 21:49:02 +0545

    update :: remote database added

--- database/index.js
+++ database/index.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
-// mongoose.connect(process.env.LOCAL_DATABASE_URI).then(function (err, done) {
-//   console.log("Database connected successfully.");
-// });
+mongoose.connect(process.env.LOCAL_DATABASE_URI).then(function (err, done) {
+  console.log("Database connected successfully.");
+});
 
-mongoose
-  .connect("mongodb://localhost:27017/resume-craft")
-  .then(function (err, done) {
-    console.log("Database connected successfully.");
-  });
+// mongoose
+//   .connect("mongodb://localhost:27017/resume-craft")
+//   .then(function (err, done) {
+//     console.log("Database connected successfully.");
+//   });
